7f084e1851656d7fa67ca16cccf79fcb238b8de9
xiaolian <2504033134@qq.com>
2018-06-09 10:19:33 +0800

    完成了UniquePaths，亦如爬楼梯那道题。主要是找到递归表达式，找到结果之间的动态关系。然后值得注意的是，这种明显递归次数很多，而且递归会产生大量重复的计算，就别用递归了。总结两道题，这种结果依赖于前面结果的题目，一般的做法都可以使用数组或者数字来暂存中间结果。最终迭代到终点。

---
+++ src/xiaolian/medium/UniquePaths.java
@@ -0,0 +1,69 @@
+package xiaolian.medium;
+
+import java.util.Arrays;
+
+public class UniquePaths {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(uniquePaths2(7, 3));
+	}
+	
+	/**
+	 * 第一个思路：
+	 * 起始这道题和前面拿到爬楼梯（只能1步和2步）的题意很像。
+	 * 其递归迭代的思路：F(m,n) = F(m-1,n) + F(m,n-1)
+	 * 其中F(x,1) = F(1,y) = 1
+	 * 因此定义一个二维矩阵来暂存各迭代过程的中间过程。
+	 * 
+	 * 这里别用递归，会产生很多无用的递归。
+	 * @param m
+	 * @param n
+	 * @return
+	 */
+    public static int uniquePaths(int m, int n) {
+        int[][] temp = new int[m][n];
+        for(int i = 0;i < m;++i) 
+        	temp[i][n - 1] = 1;
+        for(int i = 0;i < n;++i)
+        	temp[m - 1][i] = 1;
+        
+        for(int i = m - 2;i >= 0; --i) {
+        	for(int j = n - 2;j >= 0;--j) {
+        		temp[i][j] = temp[i + 1][j] + temp[i][j + 1];
+        	}
+        }
+        
+        return temp[0][0];
+    }
+    
+    /**
+     * 第二个思路：
+     * 在第一个思路的基础上，只使用一维数组来暂存数据。
+     * 
+     * @param p
+     * @param n
+     * @return
+     */
+    public static int uniquePaths2(int m, int n) {
+    	int p, q;
+    	if(m < n) {
+    		p = m;
+    		q = n;
+    	}
+    	else {
+    		p = n;
+    		q = m;
+    	}
+    	int[] temp = new int[p];
+		Arrays.fill(temp, 1);
+		for(int j = q - 2;j >= 0; --j) {
+			temp[p - 1] = 1;
+			for(int i = p - 2;i >= 0; --i) {
+				temp[i] += temp[i + 1];
+			}
+		}
+		return temp[0];
+    }
+
+}
