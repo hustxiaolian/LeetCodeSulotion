876d990ea244cd4ef9de26782fc431d017e64cbc
xiaolian <2504033134@qq.com>
2018-06-14 18:56:17 +0800

    完成了RemoveDuplicatesfromSortedList，大致思路从RemoveDuplicatesFromSortedArray学习到的，使用两个指针，只有curr>pre说明没重复，当然这个思路的正确性的前提是已经排序的。

---
+++ src/com/xiaolianhust/leetcode/easy/RemoveDuplicatesfromSortedList.java
@@ -0,0 +1,42 @@
+package com.xiaolianhust.leetcode.easy;
+
+public class RemoveDuplicatesfromSortedList {
+
+	public static class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) { val = x; }
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/**
+	 * 第一种思路：
+	 * 从之前RemoveDuplicatesFromSortedArray中discuss学习到的思想。
+	 * 在数组中使用索引，这里就光明正大的使用引用了。思路上并没有什么区别，实现起来还是比较容易的。
+	 * test1:1ms, beat 100% ε=ε=ε=┏(゜ロ゜;)┛
+	 * @param head
+	 * @return
+	 */
+    public ListNode deleteDuplicates(ListNode head) {
+    	if(head == null) return null;
+        ListNode header = new ListNode(0);
+        header.next = head;
+        
+        ListNode curr = head;
+        ListNode pre = head;
+        while(curr != null) {
+        	if(curr.val > pre.val) {
+        		pre.next = curr;
+        		pre = curr;
+        	}
+        	curr = curr.next;
+        }
+        pre.next = null;
+        return header.next;
+    }
+
+}
